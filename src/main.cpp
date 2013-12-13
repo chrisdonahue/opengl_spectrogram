adaf2db986a014ce1190e021b3999b2e53ac6a0b
cdonahue <christopherdonahue@gmail.com>
2013-12-13 14:52:11 -0600

    code works

--- src/main.cpp
+++ src/main.cpp
@@ -272,7 +272,7 @@ int main(int argc, char *argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(640, 480);
-	glutCreateWindow("3D FFT");
+	glutCreateWindow("3D Spectrogram Visualizer");
 
 	GLenum glew_status = glewInit();
 
